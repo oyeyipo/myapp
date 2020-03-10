ca9531f62cc875acd3489fe4f83870f9c005576d
Olawale Oyeyipo <olaclassik@gmail.com>
2020-03-10 12:26:47 +0100

    Updates

---
+++ precache-manifest.96cb224990ebdfb14e1aafe3eb3c9b66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d00a83a44bce9c9d2ec0b4f35a646912",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "b78366fd4b740e2d411b",
+    "url": "/myapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "495d2cf294c622ff0b7c",
+    "url": "/myapp/static/js/2.9ec97fa9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/myapp/static/js/2.9ec97fa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b78366fd4b740e2d411b",
+    "url": "/myapp/static/js/main.c8b97848.chunk.js"
+  },
+  {
+    "revision": "c7452d352d201ba7883e",
+    "url": "/myapp/static/js/runtime-main.2f5d32f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
